43256fc4c0aea326c7518fc8036e873012bfd70d
Victor Huang <103213638+vichua2006@users.noreply.github.com>
2024-11-12 13:07:10 -0500

    created generateRandGraph for testing

---
+++ utils/testing-tools.js
@@ -0,0 +1,49 @@
+import { randomIntFromInterval } from "./math-tools.js";
+
+var minPos = 100,
+  maxPos = 1000,
+  maxSize = 100,
+  minSize = 20;
+
+export function generateRandGraph(num) {
+  // generates an array of nodes and edges based on the cytoscape format
+  // to be added to a cy object
+
+  let nodes = [],
+    edges = [];
+
+  let Size = randomIntFromInterval(minSize, maxSize);
+  // gernate an array of nodes, random position and random size
+  for (let i = 0; i < num; i++) {
+    nodes.push({
+      group: "nodes",
+      data: {
+        id: i,
+        title: i.toString(),
+        size: randomIntFromInterval(minSize, maxSize), // not working
+      },
+    //   position: {
+    //     x: randomIntFromInterval(minPos, maxPos),
+    //     y: randomIntFromInterval(minPos, maxPos),
+    //   },
+      locked: false,
+    });
+  }
+
+    // generate edges with random weight
+    for (let i = 0; i < num; i++) {
+      for (let j = i + 1; j < num; j += randomIntFromInterval(0, 4)) {
+        let weight = randomIntFromInterval(minSize, maxSize);
+        edges.push({
+          group: "edges",
+          data: {
+            source: i,
+            target: j,
+            weight: weight,
+          },
+        });
+      }
+    }
+
+  return [...nodes, ...edges];
+}
