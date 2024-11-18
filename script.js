de13e08c26ac168908fefb2ec815f86750ff8f40
Victor Huang <103213638+vichua2006@users.noreply.github.com>
2024-11-18 17:14:05 -0500

    tossed everythin in main function

--- script.js
+++ script.js
@@ -2,21 +2,6 @@ let elData;
 let cy;
 let selectedNode = "";
 
-// Get elements from JSON data
-fetch("data.json")
-  .then((res) => {
-      if (!res.ok) {
-          throw new Error
-              (`HTTP error! Status: ${res.status}`);
-      }
-      return res.json();
-  })
-  .then((data)=> {
-    elData = data;
-    return data
-  })
-  .catch((error) => console.error("Unable to fetch data:", error));
-
 // Show/hide info board
 function toggleInfoBoard() {
   
@@ -77,136 +62,156 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-// Create graph after two seconds
-sleep(2000).then(() => {
-  // Create graph
-  cy = cytoscape({
-    container: document.getElementById("cy"),
-    // Graph layout
-    layout: {
-      name: 'cose',
-        animate: false,
-        animationEasing: 'ease-out',
-        animationDuration: 1000,
-        nodeRepulsion: 2000000,
-        nodeOverlap: 1000000,
-        idealEdgeLength: 1000,
-        gravity: 0,
-        initialTemp: 3000,
-        randomize: true
-    },
-    // Graph style
-    style: [
-      {
-        selector: "node",
-        style: {
-          label: "data(title)",
-          "font-family": "Trebuchet MS",
-          "font-size": function(node){return node.data("size")/10},
-          "font-weight": "bold",
-          "text-valign": "center",
-          "text-halign": "center",
-          color: "white",
-          "text-outline-color": "black",
-          "text-outline-width": 1,
-          height: "data(size)",
-          width: "data(size)",
-          "overlay-opacity": 0,
-          "background-color": "#808080",
-          "transition-property": "background-color, font-size",
-          'transition-duration': "0.1s",
-          "transition-timing-function": "ease",
-        },
-      },
-      {selector: "edge",
-        style: {
-          "overlay-opacity": 0,
-          "line-color": "#808080",
-          "width": function(edge){
-            return 1.5/edge.data("weight")
-          },
-          "overlay-opacity": 0
-        },
-      },
-      {
-        selector: 'edge:selected',
-        style: {
-          'line-color': "#808080",
+function main() {
+  // Get elements from JSON data
+  fetch("data.json")
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error
+                (`HTTP error! Status: ${res.status}`);
         }
-      },
-      {
-        selector: 'node.neighbor',
-        style: {
-          "background-color": "#ebd834",
-          "font-size": function(node){return node.data("size")*0.12},
-          "width": function (node) {return node.data("size")*1.2},
-          "height": function (node) {return node.data("size")*1.2},
-        }
-      },
-      {
-        selector: 'node.hover',
-        style: {
-          "background-color": "#adadad",
-          "font-size": function(node){return node.data("size")*0.125},
-          "width": function (node) {return node.data("size")*1.25},
-          "height": function (node) {return node.data("size")*1.25},
-        }
-      },
-      {
-        selector: 'node:selected',
-        style: {
-          "background-color": "#479aff",
-          "font-size": function(node){return node.data("size")*0.15},
-          "width": function (node) {return node.data("size")*1.5},
-          "height": function (node) {return node.data("size")*1.5},
-        }
-      }
-    ],
-    elements: elData
-  });
-
-  // Node selection and hover responses
-  cy.on("mouseover", "node", function(evt) {
-    let node = evt.target;
-    node.addClass("hover");
-  });
-  
-  cy.on("mouseout", "node", function(evt) {
-    let node = evt.target;
-    node.removeClass("hover");
-    
-  });
-  
-  cy.on("select", "node", function(evt) {
-    let node = evt.target;
-
-    // get the neighbors of the selected node and highlight them
-    let neighbors = node.neighborhood('node');
-    
-    for (let i=0;i<neighbors.length;i++){
-      neighbors[i].addClass("neighbor");
-    }  
-    
-    // Give enough time to hide board if a node is selected then redisplay board
-    sleep(250).then(()=>{
-
-      selectedNode = node.data("id");
-      toggleInfoBoard();
+        return res.json();
     })
-  });
-  
-  cy.on("unselect", "node", function(evt) {
-    selectedNode = "";
-    toggleInfoBoard();
-    let node = evt.target;
-    let neighbors = node.neighborhood('node');
+    .then((data)=> {
+      elData = data;
+      return data
+    })
+    .catch((error) => console.error("Unable to fetch data:", error));
+  // Create graph after two seconds
+  sleep(2000).then(() => {
+    // Create graph
+    cy = cytoscape({
+      container: document.getElementById("cy"),
+      // Graph layout
+      zoom: 1,
+      layout: {
+        name: 'cose',
+          animate: false,
+          animationEasing: 'ease-out',
+          animationDuration: 1000,
+          nodeRepulsion: 2000000,
+          nodeOverlap: 1000000,
+          idealEdgeLength: 1000,
+          gravity: 0,
+          initialTemp: 3000,
+          randomize: true
+      },
+      // Graph style
+      style: [
+        {
+          selector: "node",
+          style: {
+            label: "data(title)",
+            "font-family": "Trebuchet MS",
+            "font-size": function(node){return node.data("size")/10},
+            "font-weight": "bold",
+            "text-valign": "center",
+            "text-halign": "center",
+            color: "white",
+            "text-outline-color": "black",
+            "text-outline-width": 1,
+            height: "data(size)",
+            width: "data(size)",
+            "overlay-opacity": 0,
+            "background-color": "#808080",
+            "transition-property": "background-color, font-size",
+            'transition-duration': "0.1s",
+            "transition-timing-function": "ease",
+          },
+        },
+        {selector: "edge",
+          style: {
+            "overlay-opacity": 0,
+            "line-color": "#808080",
+            "width": function(edge){
+              return 1.5/edge.data("weight")
+            },
+            "overlay-opacity": 0
+          },
+        },
+        {
+          selector: 'edge:selected',
+          style: {
+            'line-color': "#808080",
+          }
+        },
+        {
+          selector: 'node.neighbor',
+          style: {
+            "background-color": "#ebd834",
+            "font-size": function(node){return node.data("size")*0.12},
+            "width": function (node) {return node.data("size")*1.2},
+            "height": function (node) {return node.data("size")*1.2},
+          }
+        },
+        {
+          selector: 'node.hover',
+          style: {
+            "background-color": "#adadad",
+            "font-size": function(node){return node.data("size")*0.125},
+            "width": function (node) {return node.data("size")*1.25},
+            "height": function (node) {return node.data("size")*1.25},
+          }
+        },
+        {
+          selector: 'node:selected',
+          style: {
+            "background-color": "#479aff",
+            "font-size": function(node){return node.data("size")*0.15},
+            "width": function (node) {return node.data("size")*1.5},
+            "height": function (node) {return node.data("size")*1.5},
+          }
+        }
+      ],
+      elements: elData
+    });
+
+    // Node selection and hover responses
+    cy.on("mouseover", "node", function(evt) {
+      let node = evt.target;
+      node.addClass("hover");
+    });
     
-    // de-highlight the neighboring nodes
-    for (let i=0;i<neighbors.length;i++){
-      neighbors[i].removeClass("neighbor");
-    }  
-  });
+    cy.on("mouseout", "node", function(evt) {
+      let node = evt.target;
+      node.removeClass("hover");
+      
+    });
+    
+    cy.on("select", "node", function(evt) {
+      let node = evt.target;
 
-  cy.autoungrabify(true);
+      // get the neighbors of the selected node and highlight them
+      let neighbors = node.neighborhood('node');
+      
+      for (let i=0;i<neighbors.length;i++){
+        neighbors[i].addClass("neighbor");
+      }  
+      
+      // Give enough time to hide board if a node is selected then redisplay board
+      sleep(250).then(()=>{
 
-})
+        selectedNode = node.data("id");
+        toggleInfoBoard();
+      })
+    });
+    
+    cy.on("unselect", "node", function(evt) {
+      selectedNode = "";
+      toggleInfoBoard();
+      let node = evt.target;
+      let neighbors = node.neighborhood('node');
+      
+      // de-highlight the neighboring nodes
+      for (let i=0;i<neighbors.length;i++){
+        neighbors[i].removeClass("neighbor");
+      }  
+    });
+
+    cy.autoungrabify(true);
+
+  })
+
+}
+
+main();
